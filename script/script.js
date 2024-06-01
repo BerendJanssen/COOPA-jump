c6a12e1062503a7d94507224051bfcb3329c022c
BerendJanssen <145255252+BerendJanssen@users.noreply.github.com>
2024-06-01 13:43:33 +0200

    PUSH 3

--- script/script.js
+++ script/script.js
@@ -1,9 +1,12 @@
 const tijd = document.querySelector('#timer')
 const gameOverTekst = document.querySelector('#gameOver')
 const gameOverInstructieTekst = document.querySelector('#gameOverInstructies')
+
 const geluidSpring = new Audio("./audio/geluidSpring.mp3") //Laten zien door Marieke in de les
 const geluidSlide = new Audio("./audio/geluidSlide.mp3")
 const geluidGameOver = new Audio("./audio/geluidGameOver.mp3")
+
+geluidSpring.volume = 0.1
 
 let pop = document.querySelector('#pop')
 let muur = document.querySelector('#muur')
@@ -16,7 +19,185 @@ const gameLoop = setInterval(function () {
     if (spelLoopt) {
         gameOver()
     }
-}, 100)
+}, 10)
+
+// Timer om elke seconde de animatie kans te berekenen
+let animatieLoop1 = setInterval(startAnimatieTot10, 2600)
+let animatieLoop2 = setInterval(startAnimatieTot20, 2100)
+let animatieLoop3 = setInterval(startAnimatieTot30, 1800)
+let animatieLoop4 = setInterval(startAnimatieTot40, 1500)
+let animatieLoop5 = setInterval(startAnimatieTot50, 1200)
+let animatieLoop6 = setInterval(startAnimatieTot60, 1000)
+let animatieLoop7 = setInterval(startAnimatieVan60, 900)
+
+// Functie om animaties willekeurig te starten t/m 15 seconden
+function startAnimatieTot10() {
+    if (spelLoopt && counter > 0 && counter <= 10) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 2.5s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 2.5')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 2500)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 2.5s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 2.5')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 2500)
+        }}    
+}
+
+function startAnimatieTot20() {
+    if (spelLoopt && counter > 11 && counter <= 20) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 2s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 2')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 2000)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 2s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 2')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 2000)
+        }}    
+}
+
+function startAnimatieTot30() {
+    if (spelLoopt && counter > 21 && counter <= 30) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 1.7s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 1.7')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 1700)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 1.7s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 1.7')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 1700)
+        }}    
+}
+
+function startAnimatieTot40() {
+    if (spelLoopt && counter > 31 && counter <= 40) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 1.4s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 1.4')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 1400)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 1.4s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 1.4')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 1400)
+        }}    
+}
+
+function startAnimatieTot50() {
+    if (spelLoopt && counter > 41 && counter <= 50) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 1.1s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 1.1')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 1100)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 1.1s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 1.1')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 1100)
+        }}    
+}
+
+function startAnimatieTot60() {
+    if (spelLoopt && counter > 51 && counter <= 60) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 0.9s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 0.9')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 900)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 0.9s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 0.9')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 900)
+        }}    
+}
+
+function startAnimatieVan60() {
+    if (spelLoopt && counter > 61) {
+        if (Math.random() < 0.5) {
+            muur.style.animation = 'obstakelSlide 0.8s linear'
+            muur.style.animationPlayState = 'running'
+            console.log('Muur komt! 0.8')
+
+            setTimeout (function() {
+                muur.style.animation = 'none'
+                muur.style.animationPlayState = 'paused'
+            }, 800)
+        }
+        else {
+            plafond.style.animation = 'obstakelSlide 0.8s linear'
+            plafond.style.animationPlayState = 'running'
+            console.log('Plafond komt! 0.8')
+
+            setTimeout (function () {
+                plafond.style.animation = 'none'
+                plafond.style.animationPlayState = 'paused'
+            }, 800)
+        }}    
+}
 
 //function voor de timer/score
 function count() {
@@ -35,17 +216,19 @@ function springen() {
     setTimeout(function () {
         pop.classList.remove('springen')
         geluidSpring.load()
-    }, 500);
+    }, 500)
 }
 
 //function om poppetje te laten sliden
 function sliden() {
     if (!pop.classList.contains('sliden')) {
         pop.classList.add('sliden')
+        pop.style.backgroundImage = 'url(./images/sliden.png)'
         geluidSlide.play();
     }
     setTimeout(function () {
         pop.classList.remove('sliden')
+        pop.style.backgroundImage = 'url(./images/pop.png)'
         geluidSlide.load()
     }, 500)
 }
@@ -94,8 +277,8 @@ function startOpnieuw() {
         plafond.style.animation = 'none'
 
         setTimeout(function () {
-            muur.style.animation = 'slide 3s linear infinite'
-            plafond.style.animation = 'slide 2.5s linear infinite'
+            muur.style.animation = ''
+            plafond.style.animation = ''
 
             muur.style.animationPlayState = 'running'
             plafond.style.animationPlayState = 'running'
@@ -103,17 +286,15 @@ function startOpnieuw() {
     }
 }
 
+
 document.addEventListener('click', startOpnieuw)
-//Event listener die keydown 'w' koppelt aan springen - https://www.toptal.com/developers/keycode handige website aangeraden door Manuel
+
+//Event listeners die de key 'w' en 's' koppelen aan springen en sliden - https://www.toptal.com/developers/keycode handige website aangeraden door Manuel
 document.addEventListener('keypress', function (event) {
     if (event.key === 'w') {
         springen()
     }
-})
-
-//Event listener die keydown 's' koppelt aan sliden
-document.addEventListener('keypress', function (event) {
-    if (event.key === 's') {
+    else if (event.key === 's') {
         sliden()
     }
 })
